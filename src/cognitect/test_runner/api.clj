a97ea6c47da400bcf72306edecff00762270e0f2
Sean Corfield <sean@corfield.org>
2024-11-16 13:38:38 -0800

    make outputs consistent
    Signed-off-by: Sean Corfield <sean@corfield.org>

--- src/cognitect/test_runner/api.clj
+++ src/cognitect/test_runner/api.clj
@@ -4,21 +4,20 @@
     [cognitect.test-runner :as tr]))
 
 (defn- do-test
-  [{:keys [dirs nses patterns vars includes excludes output]}]
+  [{:keys [dirs nses patterns vars includes excludes outputs]}]
   (let [adapted {:dir (when (seq dirs) (set dirs))
                  :namespace (when (seq nses) (set nses))
                  :namespace-regex (when (seq patterns) (map re-pattern patterns))
                  :var (when (seq vars) (set vars))
                  :include (when (seq includes) (set includes))
                  :exclude (when (seq excludes) (set excludes))
-                 :output (let [output (if (and output (symbol? output)) [output] output)]
-                              (-> (mapv #(if (qualified-symbol? %)
-                                           %
-                                           (symbol "lazytest.reporters"
-                                                   (name %)))
-                                        output)
-                                  (not-empty)
-                                  (or ['lazytest.reporters/nested])))}]
+                 :output (-> (mapv #(if (qualified-symbol? %)
+                                      %
+                                      (symbol "lazytest.reporters"
+                                              (name %)))
+                                   outputs)
+                             (not-empty)
+                             (or ['lazytest.reporters/nested]))}]
     (tr/test adapted)))
 
 (defn test
@@ -26,10 +25,11 @@
 
   * :dirs - coll of directories containing tests, default= [\"test\"]
   * :nses - coll of namespace symbols to test
-  * :patterns - coll of regex strings to match namespaces
+  * :patterns - coll of regex strings to match namespaces (clojure.test-only)
   * :vars - coll of fully qualified symbols to run tests on
   * :includes - coll of test metadata keywords to include
   * :excludes - coll of test metadata keywords to exclude
+  * :outputs - coll of LazyTest-only output reporters to use
 
   If neither :nses nor :patterns is supplied, use `:patterns [\".*-test$\"]`."
   [opts]
